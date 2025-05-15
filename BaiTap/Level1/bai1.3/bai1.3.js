33051b9d4398029613f02ad7fc4c1d11c9f4f963
tai <trantai09032003@gmail.com>
2025-05-15 08:23:32 +0700

    Bai 1.3

---
+++ BaiTap/Level1/bai1.3/bai1.3.js
@@ -0,0 +1,5 @@
+const square = (n) => {
+  return Math.pow(n, 2);
+};
+
+module.exports = square;
---
+++ BaiTap/Level1/bai1.3/bai1.3.test.js
@@ -0,0 +1,4 @@
+const square = require("./bai1.3.js");
+test("Square of 3 is 9", () => {
+  expect(square(3)).toBe(9);
+});
