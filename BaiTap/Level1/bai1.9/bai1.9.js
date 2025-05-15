29d337c3fa0203283bc1b1e0fddd7a375fbf3d29
tai <trantai09032003@gmail.com>
2025-05-15 09:51:00 +0700

    Bai 1.9

---
+++ BaiTap/Level1/bai1.9/bai1.9.js
@@ -0,0 +1,6 @@
+function numberWord(str) {
+  const strConvertToString = str.trim().split(" ");
+  return strConvertToString.length;
+}
+
+module.exports = numberWord;
---
+++ BaiTap/Level1/bai1.9/bai1.9.test.js
@@ -0,0 +1,4 @@
+const numberWord = require("./bai1.9.js");
+test("Number words", () => {
+  expect(numberWord("Hello world")).toBe(2);
+});
